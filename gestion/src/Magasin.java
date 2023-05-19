462e0882dd324a2a5a1b8010f71f3520621fdf37
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-05-19 10:32:02 +0200

    ajout de magasin

---
+++ gestion/src/Magasin.java
@@ -0,0 +1,121 @@
+package donnees;
+import donnees.CD;
+import java.util.ArrayList;
+
+/**
+ * La classe Magasin represente un magasin qui vend des CDs.</p>
+ *
+ * cette classe est caracterisee par un ensemble de CDs correspondant aux CDS
+ * vendus dans ce magasin.
+ *
+ */
+public class Magasin {
+
+    /**
+     * la liste des CDs disponibles en magasin
+     */
+    private ArrayList<CD> listeCds;
+
+    /**
+     * construit un magasin par defaut qui ne contient pas de CD
+     */
+    public Magasin() {
+        listeCds = new ArrayList<CD>();
+    }
+
+    /**
+     * ajoute un cd au magasin
+     *
+     * @param cdAAjouter
+     *            le cd a ajouter
+     */
+    public void ajouteCd(CD cdAAjouter) {
+        listeCds.add(cdAAjouter);
+    }
+
+    @Override
+    /**
+     * affiche le contenu du magasin
+     */
+    public String toString() {
+        String chaineResultat = "";
+        //parcours des Cds
+        for (int i = 0; i < listeCds.size(); i++) {
+            chaineResultat += listeCds.get(i);
+        }
+        chaineResultat += "nb Cds: " + listeCds.size();
+        return (chaineResultat);
+
+    }
+
+    /**
+     * @return le nombre de Cds du magasin
+     */
+    public int getNombreCds() {
+        return listeCds.size();
+    }
+
+    /**
+     * permet d'acceder ? un CD
+     *
+     * @return le cd a l'indice i ou null si indice est non valide
+     */
+    public CD getCd(int i)
+    {
+        CD res=null;
+        if ((i>=0)&&(i<this.listeCds.size()))
+            res=this.listeCds.get(i);
+        return(res);
+    }
+
+    // TODO ajouter methode de tri
+
+    /**
+     * permet de trier par nom d'artistes croissant
+     */
+    public void trierAriste() {
+        // tri par selection
+        int nbCDs = this.listeCds.size();
+        for (int i = 0; i < nbCDs; i++) {
+            CD cdSelectionne = this.listeCds.get(i);
+
+            //Selectionne le plus petit
+            int indiceSelection = i;
+            for (int j = i + 1; j < nbCDs; j++) {
+                CD cdTemp = listeCds.get(j);
+                if (cdTemp.etreAvantArtiste(cdSelectionne)) {
+                    indiceSelection = j;
+                    cdSelectionne = cdTemp;
+                }
+            }
+            listeCds.set(indiceSelection, listeCds.get(i));
+            listeCds.set(i, cdSelectionne);
+        }
+    }
+
+    /**
+     * permet de trier par nom d'album croissant
+     */
+    public void trierAlbum() {
+        // tri par selection
+        int nbCDs = this.listeCds.size();
+        for (int i = 0; i < nbCDs; i++) {
+            CD cdSelectionne = this.listeCds.get(i);
+
+            //selectionne plus petit
+            int indiceSelection = i;
+            for (int j = i + 1; j < nbCDs; j++) {
+                CD cdTemp = listeCds.get(j);
+                if (cdTemp.etreAvantAlbum(cdSelectionne)) {
+                    indiceSelection = j;
+                    cdSelectionne = cdTemp;
+                }
+            }
+            listeCds.set(indiceSelection, listeCds.get(i));
+            listeCds.set(i, cdSelectionne);
+        }
+    }
+
+    // FinTODO ajouter une methode de tri
+
+}
